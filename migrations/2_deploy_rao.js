29e5fdff7b72554a9c14fff6d5af089a4e312974
Girish Ramnani <girishramnani95@gmail.com>
2018-01-08 23:17:27 +0530

    added test account for integration tests

--- migrations/2_deploy_rao.js
+++ migrations/2_deploy_rao.js
@@ -3,16 +3,17 @@ var RAOToken = artifacts.require("./RAOToken.sol")
 
 module.exports = function(deployer,network,accounts) {
 
-    var ACCOUNT = "" // The mainnet account address
+    var ACCOUNT = ""; // The mainnet account address
 
 
     // currently using the first account as the multisig
     if (network == "develop") {
-        console.log("using "+accounts[0])
+        console.log("using "+accounts[0]);
         deployer.deploy(RAOToken,accounts[0]);
     }
-    else {
-        deployer.deploy(RAOToken,ACCOUNT)
+    else if (network == "rinkeby") {  // using rinkeby for integration tests
+        // for now using the first account
+        deployer.deploy(RAOToken,accounts[0]);
     }
     
 };
